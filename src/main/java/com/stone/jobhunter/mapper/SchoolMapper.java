3f01e5118f9049216f94e123f1106b9bbc9b68ce
liyue <286311613@qq.com>
2018-07-21 21:22:31 +0800

    学校

---
+++ src/main/java/com/stone/jobhunter/mapper/SchoolMapper.java
@@ -0,0 +1,7 @@
+package com.stone.jobhunter.mapper;
+
+import com.stone.jobhunter.pojo.School;
+
+public interface SchoolMapper extends BaseMapper<School>{
+
+}
---
+++ src/main/java/com/stone/jobhunter/pojo/School.java
@@ -0,0 +1,43 @@
+package com.stone.jobhunter.pojo;
+
+public class School {
+    private Integer id;
+
+    private String name;
+
+    private String type;
+
+    public School(Integer id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public School() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+}

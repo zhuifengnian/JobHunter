6a4984c0ad6055c6dbdbd0b25e845adc6c483f08
liyue <286311613@qq.com>
2018-07-23 21:22:46 +0800

    存储图片

--- src/main/java/com/stone/jobhunter/controller/weixin/WeixinResumeController.java
+++ src/main/java/com/stone/jobhunter/controller/weixin/WeixinResumeController.java
@@ -45,32 +45,7 @@ private ResumeEnterpriseService resumeEnterpriseService;
     @ApiOperation(value = "添加简历", notes = "添加简历")
     @RequestMapping(value = "/insertResume", method = RequestMethod.POST)
     public ReturnMessage insertResume(@RequestBody  String obj) throws ParseException, UnsupportedEncodingException {
-        int insert[]=new int[5];
-        obj=URLDecoder.decode(obj,"utf-8");
-        Resume resume=JsonUtil.checkJson(obj);
-        insert[0]=resumeService.insert(resume);
-        List<ResumePurpose> resumePurposeList =JsonUtil.checkJson1(obj);
-        for(ResumePurpose resumePurpose : resumePurposeList) {
-            resumePurpose.setResumeId(insert[1]);
-            insert[1] = resumePurposeService.insert(resumePurpose);
-        }
-        List<ResumeSchool>resumeSchoolList =JsonUtil.checkJson2(obj);
-        for(ResumeSchool resumeSchool: resumeSchoolList) {
-            resumeSchool.setResumeId(insert[0]);
-            insert[2] = resumeSchoolService.insert(resumeSchool);
-        }
-
-        List<ResumeScience> resumeScienceList=JsonUtil.checkJson3(obj);
-        for(ResumeScience resumeScience : resumeScienceList) {
-            resumeScience.setResumeId(insert[0]);
-            insert[3] = resumeScienceService.insert(resumeScience);
-        }
-
-       List<ResumeEnterprise> resumeEnterpriseList=JsonUtil.checkJson4(obj);
-        for(ResumeEnterprise resumeEnterprise : resumeEnterpriseList) {
-            resumeEnterprise.setResumeId(insert[0]);
-            insert[4] = resumeEnterpriseService.insert(resumeEnterprise);
-        }
+      int []insert=resumeService.putResume(obj);
         return new ReturnMessage(ResponseCode.OK,insert);
     }
     @RequestMapping(value = "/insertResumePicture", method = RequestMethod.POST)
@@ -83,6 +58,7 @@ private ResumeEnterpriseService resumeEnterpriseService;
         if (flfile != null)
             picture = QiNiuUtil.manageFile(flfile);
         resume.setUpdateTime(Calendar.getInstance().getTime());
+        resume.setUserPhone(picture);
         int insert = resumeService.updateByPrimaryKeySelective(resume);
 
         return new ReturnMessage(ResponseCode.OK, insert);

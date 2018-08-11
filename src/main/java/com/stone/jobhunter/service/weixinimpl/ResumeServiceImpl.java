52ededc7bc779b5d5957effc166f4ede5f73c542
liyue <286311613@qq.com>
2018-08-11 13:41:41 +0800

    简历上传

--- src/main/java/com/stone/jobhunter/service/weixinimpl/ResumeServiceImpl.java
+++ src/main/java/com/stone/jobhunter/service/weixinimpl/ResumeServiceImpl.java
@@ -89,34 +89,46 @@ public class ResumeServiceImpl  extends AbstractBaseServiceImpl<Resume> implemen
         }
         int insert[]=new int[6];
         Resume resume=JsonUtil.checkJson(obj);
+        if(resume.getSelfPosition().equals("0")){
+           resume.setSelfPosition("学术型");
+        }
+        else if(resume.getSelfPosition().equals("1")){
+            resume.setSelfPosition("技术型");
+        }
+        else if(resume.getSelfPosition().equals("1")){
+            resume.setSelfPosition("实践型");
+        }
+        else{
+            resume.setSelfPosition("社交型");
+        }
         insert[0]=resumeService.insert(resume);
         List<ResumePurpose> resumePurposeList =JsonUtil.checkJson1(obj);
         for(ResumePurpose resumePurpose : resumePurposeList) {
             resumePurpose.setResumeId(insert[0]);
-            insert[1] = resumePurposeService.insertSelective(resumePurpose);
+            insert[1] = resumePurposeService.insert(resumePurpose);
         }
         List<ResumeSchool>resumeSchoolList =JsonUtil.checkJson2(obj);
         for(ResumeSchool resumeSchool: resumeSchoolList) {
             resumeSchool.setResumeId(insert[0]);
 
-            insert[2] = resumeSchoolService.insertSelective(resumeSchool);
+            insert[2] = resumeSchoolService.insert(resumeSchool);
         }
 
         List<ResumeScience> resumeScienceList=JsonUtil.checkJson3(obj);
         for(ResumeScience resumeScience : resumeScienceList) {
             resumeScience.setResumeId(insert[0]);
-            insert[3] = resumeScienceService.insertSelective(resumeScience);
+            insert[3] = resumeScienceService.insert(resumeScience);
         }
 
         List<ResumeEnterprise> resumeEnterpriseList=JsonUtil.checkJson4(obj);
         for(ResumeEnterprise resumeEnterprise : resumeEnterpriseList) {
             resumeEnterprise.setResumeId(insert[0]);
-            insert[4] = resumeEnterpriseService.insertSelective(resumeEnterprise);
+            insert[4] = resumeEnterpriseService.insert(resumeEnterprise);
         }
         List<ResumeCertificate> resumeCertificateList=JsonUtil.checkJson5(obj);
         for(ResumeCertificate resumeCertificate:resumeCertificateList){
             resumeCertificate.setResumeId(insert[0]);
-            insert[5]=resumeCerficateService.insertSelective(resumeCertificate);
+            insert[5]=resumeCerficateService.insert(resumeCertificate);
         }
         return insert;
     }
